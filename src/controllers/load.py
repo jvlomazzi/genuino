7292e7d5f479ad40d961e4bc2a48a72270b407f7
João Victor Lomazzi <jvlomazzi@hotmail.com>
2021-05-17 23:10:56 -0300

    Function name correction

--- src/controllers/load.py
+++ src/controllers/load.py
@@ -7,12 +7,12 @@ class Load:
     neural_networks = []
 
     def __init__(self):
-        self.get_networks()
-        self.tokenizers(0)
-        self.models(0)
+        self.set_networks()
+        self.set_tokenizers(0)
+        self.set_models(0)
 
     # 
-    def get_networks(self):
+    def set_networks(self):
         for x in os.walk(self.path):
             folder = x[0].replace(self.path+"\\", "")
             obj = {
@@ -22,16 +22,19 @@ class Load:
             }
             if folder.find("\\")  == -1 and folder != self.path: self.neural_networks.append(obj)
     
-    def tokenizers(self, index):
+    def set_tokenizers(self, index):
         if(index < len(self.neural_networks)):
             with open(self.path + self.neural_networks[index]['name'] +'/tokenizer.pickle', 'rb') as handle:
                 self.neural_networks[index]['tokenizer'] = pickle.load(handle)
         else:
             return 1;
 
-    def models(self, index):
+    def set_models(self, index):
         if(index < len(self.neural_networks)):
             self.neural_networks[index]['model'] = load_model(self.path + self.neural_networks[index]['name'])
             return self.models(index+1)
         else:
             return 1;
+    
+    def model(self, model):
+        # model for model in neural_networks['name']: 
